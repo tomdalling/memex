8d45094727628ba62a52e4294e98f3d8155b17fe
Tom Dalling <tom@tomdalling.com>
2020-08-05 22:28:00 +1000

    Add `bin/wiki export` command
    Generates a standalone HTML file with nice styling, using pandoc.

--- lib/wiki/cli.rb
+++ lib/wiki/cli.rb
@@ -36,5 +36,29 @@ module Wiki::CLI
       end
   end
 
+  class Export < Dry::CLI::Command
+    desc "Outputs standalone HTML for a wiki page. Requires pandoc."
+    argument :page, required: true, desc: 'The name of the wiki page to export'
+
+    example 'export seinfeld_episodes'
+
+    def call(page:, args: [])
+      path = (Memex::WIKI_DIR / page).sub_ext('.md')
+      cmd = [
+        'pandoc',
+        '--from', 'markdown_github+yaml_metadata_block+header_attributes+gfm_auto_identifiers',
+        '--table-of-contents',
+        '--self-contained',
+        '--css', (Memex::ROOT_DIR / 'vendor/tufte-css/tufte.min.css').to_path,
+        *args,
+        '--',
+        path.to_path,
+      ]
+      puts Shellwords.join(cmd)
+      exec(*cmd)
+    end
+  end
+
   register "open", Open
+  register "export", Export
 end
