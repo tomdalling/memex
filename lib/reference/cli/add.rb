6c78e9bc7985117b800a31820107f52664e0d278
Tom Dalling <tom@tomdalling.com>
2020-08-22 11:36:06 +1000

    Use date as basename for reference docs

--- lib/reference/cli/add.rb
+++ lib/reference/cli/add.rb
@@ -7,20 +7,19 @@ module Reference
     def initialize(
       file_system: FileSystem,
       config: Config.instance,
-      random: Random.new,
       now: Time.method(:now),
       fulltext_extractor: FulltextExtractor
     )
       @file_system = file_system
       @config = config
-      @random = random
       @now = now
       @fulltext_extractor = fulltext_extractor
     end
 
-    def call(files:, tags: [])
-      files.each do |input_path|
-        run(input_path: Pathname(input_path), tags: tags)
+    def call(files:, tags: nil)
+      files.map{ Pathname(_1) }.each do |input_path|
+        metadata = metadata_for(input_path, tags)
+        add_document(input_path, metadata)
       end
     end
 
@@ -29,39 +28,41 @@ module Reference
       BASENAME_LEN = 4
       VALID_FILENAME_CHARS = ('a'..'z').to_a + ('0'..'9').to_a
 
-      def run(input_path:, tags:)
-        ref_path = random_new_ref_path(input_path.extname)
+      def metadata_for(original_path, tags)
+        {}.tap do |metadata|
+          metadata[:added_at] = @now.().iso8601
+          metadata[:original_filename] = original_path.basename
+          metadata[:tags] = tags if tags
+        end
+      end
+
+      def add_document(input_path, metadata)
+        ref_path = generate_ref_path(input_path.extname)
         @file_system.copy(input_path, ref_path)
-        write_metadata(ref_path, input_path, tags)
+        write_metadata(ref_path, metadata)
         write_fulltext(ref_path)
       end
 
-      def random_new_ref_path(ext)
-        500.times do
-          path = @config.reference_dir.join(random_basename).sub_ext(ext)
-          return path unless @file_system.exists?(path)
-        end
-
-        fail "Couldn't generate new unique reference path"
-      end
-
-      def random_basename
-        "".tap do |basename|
-          BASENAME_LEN.times do
-            idx = @random.rand(VALID_FILENAME_CHARS.size)
-            basename << VALID_FILENAME_CHARS[idx]
+      def generate_ref_path(ext)
+        date = @now.().to_date.iso8601
+        (1..).each do |suffix|
+          basename = "#{date}_#{suffix.to_s.rjust(3, '0')}"
+          unless basename_exists?(basename)
+            return @config.reference_dir.join(basename).sub_ext(ext)
           end
         end
       end
 
-      def write_metadata(ref_path, original_path, tags)
+      def basename_exists?(basename)
+        @file_system.children_of(@config.reference_dir).any? do
+          _1.basename.to_s.start_with?(basename)
+        end
+      end
+
+      def write_metadata(ref_path, metadata)
         @file_system.write(
           ref_path.sub_ext('.metadata.yml'),
-          YAML.dump(deep_yamlify({
-            added_at: @now.().iso8601,
-            original_filename: original_path.basename,
-            tags: tags,
-          }))
+          YAML.dump(deep_yamlify(metadata)),
         )
       end
 
--- test/automated/reference/cli/add.rb
+++ test/automated/reference/cli/add.rb
@@ -1,31 +1,37 @@
 RootContext.context Reference::CLI::Add do
 
-  fs = FileSystemFake.new("/in/greetings.txt" => "hello")
+  fs = FileSystemFake.new(
+    "/in/greetings.txt" => "hello",
+    "/ref/2020-12-25_001.png" => "filename taken",
+  )
   subject = class_under_test.new(
     file_system: fs,
     config: Struct.new(:reference_dir).new(Pathname('/ref')),
-    random: RandomFake.new,
     now: ->() { Time.iso8601('2020-12-25T09:00:00+10:00') },
     fulltext_extractor: ->(path:, file_system:) { 'full text here' },
   )
 
   subject.call(files: ["/in/greetings.txt"], tags: %w(a b c))
 
-  test "copies the file into the correct folder, using a randomly generated filename" do
-    assert_eq(fs.read('/ref/aaaa.txt'), 'hello')
+  test "does not affect existing ref files" do
+    assert_eq(fs.read('/ref/2020-12-25_001.png'), 'filename taken')
+  end
+
+  test "generates a unique filename based on the time and existing files" do
+    assert_eq(fs.read('/ref/2020-12-25_002.txt'), 'hello')
   end
 
   test "extracts full text into a sidecar file" do
-    assert_eq(fs.read('/ref/aaaa.fulltext.txt'), 'full text here')
+    assert_eq(fs.read('/ref/2020-12-25_002.fulltext.txt'), 'full text here')
   end
 
   test "writes metadata into a separate file" do
-    assert(fs.exists?('/ref/aaaa.metadata.yml'))
+    assert(fs.exists?('/ref/2020-12-25_002.metadata.yml'))
   end
 
   context 'metadata' do
     metadata = YAML.safe_load(
-      fs.read('/ref/aaaa.metadata.yml'),
+      fs.read('/ref/2020-12-25_002.metadata.yml'),
       symbolize_names: true,
     )
 
